d253ed8c89cb6fd51920cce024e7b6ffdd8bd49f
Mauricio Salmoran <70659429+s11653565@users.noreply.github.com>
2022-01-20 09:26:10 -0500

    5B
    Updated to function now

---
+++ .github/JacksonStanton00.java
@@ -0,0 +1,9 @@
+
+
+public class JacksonStanton00 { 
+    public static void main(String[] args) {
+        String testString[] = new String[10];
+        int testInt[] = new int[10];
+        double testDouble[] = new double[10];
+    }
+} 
