07371f6cab9a1fd560104df01390a060042aaa12
Mauricio Salmoran <70659429+s11653565@users.noreply.github.com>
2022-01-24 08:49:05 -0500

    5B v0.2
    Added Mr. Kelly's way. Array's. String, int, double

--- .github/JacksonStanton00.java
+++ .github/JacksonStanton00.java
@@ -1,9 +1,34 @@
+// Mauricio Salmoran, Arrays practice, v0.2, 01/24/22
 
-
-public class JacksonStanton00 { 
+// Stantons Way =
+/* public class JacksonStanton00 { 
     public static void main(String[] args) {
         String testString[] = new String[10];
         int testInt[] = new int[10];
         double testDouble[] = new double[10];
     }
-} 
+} */
+
+
+// Mr.Kelly Way
+public class JacksonStanton00 {
+
+
+    public static void main(String[] args) { 
+
+        String[] carArr = {"BMW M5", "Ferrari 250 GTO", "Porsche 911", "Ford F150 Lightning", "Buick Skylark"}; 
+        int[] myGrades = {100, 25, 15, -3, 0, 75, 35};
+        double[] myBankAccount = {0.0, -1.25, 3.75, 456.99, 25.25, 12.123456, 3.14};
+
+    }
+
+
+
+
+
+
+
+
+
+
+}
